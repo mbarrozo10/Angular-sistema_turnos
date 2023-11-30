ad036b4a40d21f3d55132f611aa089d972417f04
Matias Barrozo <matias.barrozo98@gmail.com>
2023-11-30 19:18:51 -0300

    directibvas!

--- src/app/pipes/fecha-dos.pipe.ts
+++ src/app/pipes/fecha-dos.pipe.ts
@@ -16,7 +16,7 @@ export class FechaDosPipe implements PipeTransform {
     const month = parsedDate.getMonth() + 1; 
     const year = parsedDate.getFullYear();
 
-    const formattedDate = `${day.toString().padStart(2, '0')}/${month.toString().padStart(2, '0')}/${year}`;
+    const formattedDate = `${day.toString().padStart(2, '0')}-${month.toString().padStart(2, '0')}-${year}`;
 
     return formattedDate;
   }
